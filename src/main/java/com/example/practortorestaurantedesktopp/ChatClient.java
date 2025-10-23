a82c1203a3fe44da54e800fb66b335d3bb4ca2fd
1001inchNails <1001inchnails@gmail.com>
2025-10-23 15:23:47 +0200

    Initial commit

---
+++ src/main/java/com/example/practortorestaurantedesktopp/ChatClient.java
@@ -0,0 +1,2 @@
+package com.example.practortorestaurantedesktopp;public class ChatClient {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.practortorestaurantedesktopp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.practortorestaurantedesktopp to javafx.fxml;
+    exports com.example.practortorestaurantedesktopp;
+}
